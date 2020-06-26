7fe89414cba2fdffd14ae32cf0ca8168af618562
Rodrigo Castilho <rodrigo.m.castilho@outlook.com>
2020-06-26 15:11:16 -0300

    Adding contact buttons, API

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Feather } from '@expo/vector-icons';
 import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import logoImg from '../../assets/logo.png';
 import styles from './styles';
+import api from '../../services/api';
 
 export default function Incidents() {
     const navigation = useNavigation();
 
     function navigateToDetail() {
         navigation.navigate('Detail');
+    }
+
+    async function loadIncidents() {
+        const [ incidents, setIncidents ] = useState([]);
+        const response = await api.get('incidents');
     }
 
     return (
